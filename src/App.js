d3c4330099f1b5c3c619330b59206971ca891523
Jomel Bautista <jomelgbautista@gmail.com>
2023-01-14 21:17:48 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// Components imports
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import EditPost from "./components/EditPost";
+import About from "./components/About";
+import Missing from "./components/Missing";
+
+// React hooks
+import { useEffect } from "react";
+
+// React-router-dom
+import { Route, Switch } from "react-router-dom";
+
+// Custom react hooks
+import useAxiosFetch from "./hooks/useAxiosFetch";
+
+// Easy Peasy
+import { useStoreActions } from "easy-peasy";
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts)
+
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts"
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* DataProvider goes around all components so that data can be accessed (i.e. components can subscribe to DataProvider data) */}
+      <Header title="React JS Blog" />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home isLoading={isLoading} fetchError={fetchError} />
+        </Route>
+        <Route exact path="/post" component={NewPost} />
+        <Route path="/edit/:id" component={EditPost} />
+        <Route path="/post/:id" component={PostPage} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+// React icons
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
+
+// Custom hook
+import useWindowSize from "../hooks/useWindowSize";
+
+const Header = ({ title }) => {
+  const { width } = useWindowSize()
+
+  return (
+    <header className="Header">
+      <h1>{title}</h1>
+      {width < 768 ? (
+        <FaMobileAlt />
+      ) : width < 992 ? (
+        <FaTabletAlt />
+      ) : (
+        <FaLaptop />
+      )}
+    </header>
+  );
+};
+
+export default Header;
